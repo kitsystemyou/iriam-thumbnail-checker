6798b496c7587d9f4074b324e8722c18679603a6
kitsystemyou <kitsystemyou@gmail.com>
2023-05-18 02:25:35 +0900

    Feat: アップロードされた画像にアイコンを重ねて表示

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState } from 'react';
 
 function App() {
+  const imageWidth = 513; // IRIAM 推奨サイズ
+  const [uploadImage, setUploadImage] = useState('default.png');
+  const onFileInputChange = (e) => {
+    if (!e.target.files) return;
+
+    // ファイルを取得
+    const fileObject = e.target.files[0];
+    // オブジェクトURLを生成し、useState()を更新
+    // ファイル選択でキャンセルを押下された場合はsrcを更新しない
+    if (fileObject === undefined){
+      return;
+    }
+    setUploadImage(window.URL.createObjectURL(fileObject));
+  };
+
   return (
     <div className="App">
+      <h1>IRIAIM サムネイルチェッカー</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Outer'>
+        <img src={uploadImage} className="InputFile" alt="logo" width={imageWidth}/>
+        <img src="live.png" className='Live' alt='live'></img>
+        <img src="score.png" className='Score' alt='score'></img>
+        </div>
+        <input
+          type='file'
+          accept="image/*"
+          onChange={onFileInputChange}
+        />
+        <p>※正方形の画像しか対応していませんので<br />ご了承くださいm(_ _)m</p>
       </header>
     </div>
   );
