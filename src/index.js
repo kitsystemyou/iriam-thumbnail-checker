f182590868fa4853a97047ad8c01c1ea8591f05e
kitsystemyou <kitsystemyou@gmail.com>
2023-05-21 16:39:42 +0900

    Add Sentry monitor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://e749929eaa0947fb9c12dc13de05bfb7@o4505216519897088.ingest.sentry.io/4505216531169280",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
