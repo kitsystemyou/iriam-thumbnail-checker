5479bda03edae1ce03e38720ee1d21bf5483809f
kitsystemyou <kitsystemyou@gmail.com>
2023-05-21 19:28:20 +0900

    Feat: donuts switch button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { React, useState } from 'react';
 function App() {
   const imageWidth = 513; // IRIAM 推奨サイズ
   const [uploadImage, setUploadImage] = useState('default.png');
+  const [visibility, setVisibility] = useState('visible');
   const onFileInputChange = (e) => {
     if (!e.target.files) return;
 
@@ -18,6 +19,15 @@ function App() {
     setUploadImage(window.URL.createObjectURL(fileObject));
   };
 
+  const switchDonuts = () => {
+    const donuts = document.getElementsByClassName('Donuts')[0];
+    if (donuts.style.visibility === 'visible'){
+      setVisibility('hidden');
+    } else {
+      setVisibility('visible');
+    }
+  }
+
   return (
     <div className="App">
       <h1>IRIAIM サムネイルチェッカー</h1>
@@ -26,13 +36,14 @@ function App() {
         <img src={uploadImage} className="InputFile" alt="logo" width={imageWidth}/>
         <img src="live.png" className='Live' alt='live'></img>
         <img src="score.png" className='Score' alt='score'></img>
-        <img src="donuts.png" className='Donuts' alt='donuts'></img>
+        <img src="donuts.png" className='Donuts' alt='donuts' style={{visibility: visibility}}></img>
         </div>
         <input
           type='file'
           accept="image/*"
           onChange={onFileInputChange}
         />
+        <button onClick={switchDonuts}>ドーナツON/OFF</button>
         <p>※正方形の画像しか対応していませんので<br />ご了承くださいm(_ _)m</p>
       </header>
     </div>
